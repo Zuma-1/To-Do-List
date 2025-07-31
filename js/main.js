5bd66f704a2f47ba956586deb61ed80a7ea1503f
= <moabdelaziem@gmail.com>
2025-07-31 17:17:38 +0300

    file updated

--- js/main.js
+++ js/main.js
@@ -120,6 +120,7 @@ window.addEventListener("load",function(){
 this.document.getElementById("upSpinner").classList.remove("d-flex");
 this.document.getElementById("upSpinner").classList.add("d-none");
 this.document.getElementById("upParent").classList.remove("d-none");
+saveToLocalStorage()
 })
 
 
